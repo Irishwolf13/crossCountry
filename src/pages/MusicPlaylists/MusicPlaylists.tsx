d0492b43aa01077c294a882736d4c85c078c7d60
John Rooney <rooneyjohn@gmail.com>
2025-08-01 11:47:26 -0400

    small music changes

--- src/pages/MusicPlaylists/MusicPlaylists.tsx
+++ src/pages/MusicPlaylists/MusicPlaylists.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 import './MusicPlaylists.css';
@@ -6,11 +6,17 @@ import '../../theme/variables.css';
 
 const MusicPlaylists: React.FC = () => {
   const history = useHistory();
+  const [currentPlaylist, setCurrentPlaylist] = useState("3sz9bL604aa9Uy9U8Z7xmj");
 
   const goToHome = () => {
     history.push('/home');
   };
-
+  
+  const playlists = {
+    playlist1: "3sz9bL604aa9Uy9U8Z7xmj",
+    playlist2: "72Uoqg5XweiivRRYFpBa3z",
+  };
+  
   return (
     <IonPage>
       <IonHeader>
@@ -18,17 +24,33 @@ const MusicPlaylists: React.FC = () => {
           <IonButtons slot="start">
             <IonButton className="mapPageButton" onClick={goToHome}>Home</IonButton>
           </IonButtons>
-          <IonTitle  style={{color: '#f7870f'}}>Road Tunes</IonTitle>
+          <IonTitle style={{ color: '#f7870f' }}>Road Tunes</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <iframe 
-            src="https://open.spotify.com/embed/playlist/6G3DO2p33GcehtrJGYKJgA?utm_source=generator" 
+      <IonContent>
+        <div className='selectionHolder'>
+          <div>Select Playlist:</div>
+          <div className='dropdownHolder'>
+            <select 
+              value={currentPlaylist} 
+              onChange={(e) => setCurrentPlaylist(e.target.value)}
+              className='playlistDropdown'
+            >
+              <option value={playlists.playlist1}>Hack the Highway</option>
+              <option value={playlists.playlist2}>Hack the Pubs</option>
+            </select>
+          </div>
+
+        </div>
+        <div className='musicHolder'>
+          <iframe
+            src={`https://open.spotify.com/embed/playlist/${currentPlaylist}?utm_source=generator`}
             width="100%"
-            height= '100%'
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            height="100%"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy">
-        </iframe>
+          </iframe>
+        </div>
       </IonContent>
     </IonPage>
   );
